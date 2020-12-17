ad1bba252595dead0dbfccf2dc2192d1cf7866ce
Manik <mggmanik@gmail.com>
2020-12-17 17:33:36 +0530

    angular material and bootstrap

---
+++ src/app/angular-material.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '@angular/core';
+// provide angular material modules
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatTableModule } from '@angular/material/table';
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatSnackBarModule,
+    MatSelectModule,
+    MatChipsModule,
+    MatDialogModule,
+    MatDatepickerModule,
+    MatButtonModule,
+    MatAutocompleteModule,
+    MatStepperModule,
+    MatPaginatorModule,
+    MatMenuModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatDividerModule,
+    MatMenuModule,
+    MatListModule,
+    MatSlideToggleModule,
+    MatExpansionModule,
+    MatTableModule,
+  ],
+})
+export class AngularMaterialModule { }
