e8005d58704bbc2bd0f9929a35572d256162e8a3
Manik <mggmanik@gmail.com>
2020-12-20 02:24:54 +0530

    added test cases for api service

--- src/app/home/api.service.spec.ts
+++ src/app/home/api.service.spec.ts
@@ -1,6 +1,7 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { inject, TestBed } from '@angular/core/testing';
 import { ApiService } from './api.service';
+import { environment } from '../../environments/environment';
 
 
 describe('ApiService', () => {
@@ -17,4 +18,59 @@ describe('ApiService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should list all the filters by filtername',
+    inject([HttpTestingController],
+      (httpMock: HttpTestingController) => {
+        service.listFilterByFilterName('c').subscribe((res) => {
+          expect(res.drinks.length).toBeGreaterThan(1);
+        });
+        const mockRequest = httpMock.expectOne(`${environment.apiUrl}/list.php?c=list`);
+        expect(mockRequest.request.method).toEqual('GET');
+      })
+  );
+
+  it('should list all the drinks by filter',
+    inject([HttpTestingController],
+      (httpMock: HttpTestingController) => {
+        service.fetchDrinksBySelectedFilter('c', 'Cocktail').subscribe((res) => {
+          expect(res.drinks.length).toBeGreaterThan(1);
+        });
+        const mockRequest = httpMock.expectOne(`${environment.apiUrl}/filter.php?c=Cocktail`);
+        expect(mockRequest.request.method).toEqual('GET');
+      })
+  );
+
+  it('should search drinks with a searchvalue',
+    inject([HttpTestingController],
+      (httpMock: HttpTestingController) => {
+        service.searchDrinks('Cocktail').subscribe((res) => {
+          expect(res.drinks.length).toBeGreaterThan(1);
+        });
+        const mockRequest = httpMock.expectOne(`${environment.apiUrl}/search.php?s=Cocktail`);
+        expect(mockRequest.request.method).toEqual('GET');
+      })
+  );
+
+  it('should search drinks with a letter of first word',
+    inject([HttpTestingController],
+      (httpMock: HttpTestingController) => {
+        service.searchDrinksByFirstLetter('A').subscribe((res) => {
+          expect(res.drinks.length).toBeGreaterThan(1);
+        });
+        const mockRequest = httpMock.expectOne(`${environment.apiUrl}/search.php?f=A`);
+        expect(mockRequest.request.method).toEqual('GET');
+      })
+  );
+
+  it('should fetch drink by ID',
+    inject([HttpTestingController],
+      (httpMock: HttpTestingController) => {
+        service.fetchDrinkById('15346').subscribe((res) => {
+          expect(res.drinks.length).toBeGreaterThan(1);
+        });
+        const mockRequest = httpMock.expectOne(`${environment.apiUrl}/lookup.php?i=15346`);
+        expect(mockRequest.request.method).toEqual('GET');
+      })
+  );
 });
