d2dc1704b72e8cbe58511f9b3f8727901d34b185
Manik <mggmanik@gmail.com>
2020-12-19 23:14:48 +0530

    unit test for drinks component

--- src/app/home/drinks/drinks.component.spec.ts
+++ src/app/home/drinks/drinks.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AngularMaterialModule } from './../../angular-material.module';
@@ -32,4 +33,10 @@ describe('DrinksComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have heading as Welcome to the Cocktail App ', () => {
+    const heading = fixture.debugElement.nativeElement.querySelector('#firstHeading');
+    expect(heading.innerHTML).toBe('Welcome to the Cocktail App');
+  });
+
 });
